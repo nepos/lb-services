00f6798e48854d6620ec0cf2b73bee629c83616f
uiftikhar <umer229@gmail.com>
2016-10-27 12:17:38 +0200

    version 1.0.2 test

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,3 @@ var runSequence = require('run-sequence');
 var brancher = require('gulp-brancher')(gulp, 'package.json');
 
 // Pull the lb services
-gulp.task('pull-lbservices', function(callback) {
-	exec('git pull', function(err, stdout, stderr) {
-		if(err || stderr) {
-			callback(err || stderr);
-			return;
-		}
-		callback(null, true);
-	});
-});
-
-gulp.task('update-lbservices', function(callback) {
-	runSequence('pull-lbservices', 'bump-version', 'add', 'commit', callback);
-});
