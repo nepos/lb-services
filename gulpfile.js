34b504375dc8172e2e239c8d869732f94e837397
uiftikhar <umer229@gmail.com>
2016-10-27 14:38:13 +0200

    version 1.0.4 test 3

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,4 @@
 /* global require */
 var gulp = require('gulp');
-var exec = require('child_process').exec;
-var runSequence = require('run-sequence');
 var brancher = require('gulp-brancher')(gulp, 'package.json');
 
-// Pull the lb services
